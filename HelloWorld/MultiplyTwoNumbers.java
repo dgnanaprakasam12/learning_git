6cbefee0efced7793bffa6f09a04763301cd9a67
Gnanaprakasam Dhayanantham <dgprakash@gmail.com>
2025-02-10 16:10:56 +0530

    empty spae for testuing

--- HelloWorld/MultiplyTwoNumbers.java
+++ HelloWorld/MultiplyTwoNumbers.java
@@ -7,6 +7,9 @@ public class MultiplyTwoNumbers {
 
         float product = first * second;
 
-        System.out.println("The product is: " + product);
+        
+		
+		System.out.println("The product is: " + product);
+		
     }
 }
